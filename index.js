f612adf688008c7d9ec8365dbd1b09f37ecba98f
abhi2002dhi <abhi2002dhiman@gmail.com>
2024-08-01 16:26:50 +0530

    frontend-server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import Scores from './models/scores.js';
 
 const app = express();
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect("mongodb+srv://abhi2002dhi:nidhidhiman@cluster0.kp6ro.mongodb.net/hummingBee?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
